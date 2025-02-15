2ad842024b541b8c73edca9e0465bf9889e740cc
isaias-dev-7 <isaiasosoria7@gmail.com>
2025-02-15 00:07:37 -0600

    firts commit

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true
+      }
+    })
+  )
+
+  const config = new DocumentBuilder()
+    .setTitle('STI-BACK')
+    .setDescription('API for STI-BACK project')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3000, () => {
+    console.log(`http://localhost:${process.env.PORT}/docs`);
+    console.log(`http://localhost:${process.env.PORT}/api`);
+  });
+}
+bootstrap();
